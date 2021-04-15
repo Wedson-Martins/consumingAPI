92a75c1a975f5c1fcb30d1d6be4f1a48f47d33ad
Wedson Martins <wedsoncontato@gmail.com>
2021-04-15 06:22:12 -0300

    consuming API ViaCep

---
+++ consuming_api/src/test/java/br/com/wcode/consuming_api/ConsumingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wcode.consuming_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
